6a49fe8ef896fce48aca923d7052602041db1655
Sujay Muramalla <sujay.muramalla@gmail.com>
2025-11-17 10:39:57 +0100

    added new files for class12

---
+++ app.js
@@ -0,0 +1,41 @@
+var a = 10;
+var b = 20;
+var c = 20;
+var d = 20;
+var e = 20;
+var f = 20;
+var g = 20;
+
+
+
+function add(a, b) {
+    return (a + b);
+
+}
+function sub(c, d) {
+    return (Math.abs(c - d));
+}
+function mul(e, f) {
+    return (e * f);
+}
+function div(g, h) {
+    return (g / h);
+}
+
+
+
+var res_add = add(a, b);
+console.log(res_add);
+
+var res_sub = sub(a, b);
+console.log(res_sub);
+
+var res_mul = mul(a, b);
+console.log(res_mul);
+
+
+var res_div = div(a, b);
+console.log(res_div);
+
+
+
